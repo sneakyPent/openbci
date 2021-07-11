a024b68713ec1143600cdfecf3d6a01d9de5153a
sneakyPent <zaharioudakis@yahoo.com>
2021-07-12 02:27:08 +0300

    rename some variables on dataManager.py

--- source/dataManager.py
+++ source/dataManager.py
@@ -3,12 +3,12 @@ from multiprocessing import Process
 
 class DataManager(object):
     """
-        Used to share data from one queue to every other queue contained in the given processesQueuesList
+        Used to _share data from one queue to every other queue contained in the given processesQueuesList
 
         Args:
             data: queue type data will be shared
             processesArgsList: A list of dictionaries with queues the data will get into and their locks
-            _share: Event used to wait when there is no data to share
+            _share: Event used to wait when there is no data to _share
             _newDataAvailable: Event used to inform ,for new data, every other process that is using at least one of
                                 the queues contained in processesQueuesList
         """
@@ -16,31 +16,34 @@ class DataManager(object):
     def __init__(self, data, processesArgsList, _share, _newDataAvailable):
         self.data = data
         self.processesArgsList = processesArgsList
-        self.share = _share
-        self.newDataAvailable = _newDataAvailable
+        self._share = _share
+        self._newDataAvailable = _newDataAvailable
 
     def disableNewDataAvailable(self):
         while True:
             NonEmpty = any(proc.queue.qsize() > 0 for proc in self.processesArgsList)
             if not NonEmpty:
-                self.newDataAvailable.clear()
-                self.share.wait()
+                self._newDataAvailable.clear()
+                self._share.wait()
 
     def shareData(self):
+        print("shareData: waiting")
+        print("shareData size: " + self.data.qsize().__str__())
         p = Process(target=self.disableNewDataAvailable)
         p.start()
         try:
             while True:
-                self.share.wait()
+                self._share.wait()
                 while not self.data.empty():
                     dt = self.data.get()
+                    print("_share Data func:" + dt.__str__())
                     for procArgs in self.processesArgsList:
                         procArgs.lock.acquire()
                         try:
                             procArgs.queue.put(dt)
                         finally:
                             procArgs.lock.release()
-                    self.newDataAvailable.set()
-                self.share.clear()
+                    self._newDataAvailable.set()
+                self._share.clear()
         except KeyboardInterrupt:
             p.terminate()
