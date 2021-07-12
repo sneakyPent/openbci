2c29a2da3aa303ce058e50f2176b9b969d4dd5c2
sneakyPent <zaharioudakis@yahoo.com>
2021-07-12 17:51:26 +0300

    reformat boardEventHandler.py

--- source/boardEventHandler.py
+++ source/boardEventHandler.py
@@ -2,39 +2,39 @@ from multiprocessing import Process
 
 
 class BoardEventHandler:
-    def __init__(self, board, boardApiCallEvents, boardSettingsQueue):
-        self.board = board
-        self.boardApiCallEvents = boardApiCallEvents
-        self.boardSettings = boardSettingsQueue.get
+ 	def __init__(self, board, boardApiCallEvents, boardSettings):
+		self.board = board
+		self.boardApiCallEvents = boardApiCallEvents
+		self.boardSettings = boardSettings
 
-    def connect(self):
-        ev = self.boardApiCallEvents.connect
-        while True:
-            ev.wait()
-            self.board.connect()
-            ev.clear()
+	def connect(self):
+		ev = self.boardApiCallEvents.connect
+		while True:
+			ev.wait()
+			self.board.connect()
+			ev.clear()
 
-    def disconnect(self):
-        ev = self.boardApiCallEvents.disconnect
-        while True:
-            ev.wait()
-            print("disconnect")
-            self.board.disconnect()
-            ev.clear()
+	def disconnect(self):
+		ev = self.boardApiCallEvents.disconnect
+		while True:
+			ev.wait()
+			print("disconnect")
+			self.board.disconnect()
+			ev.clear()
 
-    def startStreaming(self):
-        ev = self.boardApiCallEvents.startStreaming
-        while True:
-            ev.wait()
-            print("startStreaming")
-            ev.clear()
+	def startStreaming(self):
+		ev = self.boardApiCallEvents.startStreaming
+		while True:
+			ev.wait()
+			print("startStreaming")
+			ev.clear()
 
-    def stopStreaming(self):
-        ev = self.boardApiCallEvents.stopStreaming
-        while True:
-            ev.wait()
-            print("stopStreaming")
-            ev.clear()
+	def stopStreaming(self):
+		ev = self.boardApiCallEvents.stopStreaming
+		while True:
+			ev.wait()
+			print("stopStreaming")
+			ev.clear()
 
     def newBoardSettingsAvailable(self):
         ev = self.boardApiCallEvents.newBoardSettingsAvailable
