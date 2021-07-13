5623bb54bc9c93c43c63c584ffa4b66f166c210b
sneakyPent <zaharioudakis@yahoo.com>
2021-07-14 00:43:37 +0300

    create some methods for printing messages with colors in coloringPrint.py

---
+++ utils/coloringPrint.py
@@ -0,0 +1,29 @@
+from utils.constants import Constants as cnst
+
+
+def printError(string):
+	print(cnst.FAIL + 'ERROR: ' + cnst.ENDC + string)
+
+
+def printSuccess(string):
+	print(cnst.OKGREEN + 'SUCCESS: ' + cnst.ENDC + string)
+
+
+def printWarning(string):
+	print(cnst.WARNING + 'WARNING: ' + cnst.ENDC + string)
+
+
+def printHeader(string):
+	print(cnst.HEADER + string + cnst.ENDC)
+
+
+def printInfo(string):
+	print(cnst.CYAN + 'INFO: ' + cnst.ENDC + string)
+
+
+def printBold(string):
+	print(cnst.BOLD + string + cnst.ENDC)
+
+
+def printUnderlined(string):
+	print(cnst.UNDERLINE + string + cnst.ENDC)
