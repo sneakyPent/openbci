5eeaef19dfd41db3715230f9d396f7d9279baae5
sneakyPent <zaharioudakis@yahoo.com>
2021-07-22 18:21:27 +0300

    * removing dataManager logic from UIManager.py.     - not really needed     - excessive usage of cpu core     - delay in the data sharing     - occasionally, the process could not stop
    * pass the dataBuffer that will contain the streaming data in the boardEventHandler
    
    * Added windowingData process
    
    * Remove all locks for the queue buffers

--- source/UIManager.py
+++ source/UIManager.py
@@ -4,11 +4,12 @@ import os
 import signal
 
 from pyGUI import *
-from dataManager import DataManager
 from multiprocessing.managers import SyncManager
 from multiprocessing import Process, Queue, Lock, Event, current_process
 from dotted.collection import DottedDict
 from boardEventHandler import BoardEventHandler
+from source.windowing import windowing
+from utils.coloringPrint import printWarning
 from writeToFile import writing
 from cyton import OpenBCICyton
 
@@ -28,7 +29,9 @@ processesList = []
 # main events
 writeDataEvent = Event()
 shutdownEvent = Event()
-closeGui = Event()
+newDataAvailable = Event()
+
+windowedDataBuffer = Queue(maxsize=writeDataMaxQueueSize)
 
 
 # create a SyncManager and register openbci cyton board object so as to create a proxy and share it to every subprocess
@@ -40,18 +43,13 @@ class MyManager(SyncManager):
 MyManager.register('OpenBCICyton', OpenBCICyton)
 
 
-def printData(br, dataDict, _newDataAvailable, _shutdownEvent):
+def printData(data, _newDataAvailable, _shutdownEvent):
 	while not _shutdownEvent.is_set():
 		_newDataAvailable.wait(1)
 		if _newDataAvailable.is_set():
-			b = br
-			while not dataDict.queue.empty():
-				dataDict.lock.acquire()
-				try:
-					dt = dataDict.queue.get()
-					print(dt)
-				finally:
-					dataDict.lock.release()
+			while not data.qsize() == 0:
+				dt = data.get()
+				print(dt)
 
 
 def signal_handler(signal, frame):
@@ -70,7 +68,6 @@ if __name__ == '__main__':
 	manager = MyManager()
 	manager.start()
 	board = manager.OpenBCICyton()
-	dataBuffer = manager.Queue(maxsize=maxQueueSize)
 
 	# add the board settings in the boardCytonSettings will be given to the boardEventHandler and guiProcess
 	# Through this dictionary, the board settings given from ui, will be applied to board data
@@ -78,48 +75,46 @@ if __name__ == '__main__':
 
 	# main queue that will read data from board
 	guiProcArgs = DottedDict({"queue": Queue(maxsize=maxQueueSize), "lock": Lock()})
-	printDataProcArgs = DottedDict({"queue": Queue(maxsize=maxQueueSize), "lock": Lock()})
-	writeDataProcArgs = DottedDict({"queue": Queue(maxsize=maxQueueSize * 100), "lock": Lock()})
+	printBuffer = Queue(maxsize=maxQueueSize)
+	writingBuffer = Queue(maxsize=writeDataMaxQueueSize)
+	windowingBuffer = Queue(maxsize=writeDataMaxQueueSize)
 	# add queue and lock in the lists
-	# processesArgsList = [guiProcArgs, printDataProcArgs, writeDataProcArgs]
-	# processesArgsList = [printDataProcArgs, writeDataProcArgs]
-	processesArgsList = [writeDataProcArgs]
-	# processesArgsList = [printDataProcArgs]
+	dataBuffersList = [writingBuffer, windowingBuffer, printBuffer]
 
-	# Create a DataManager Instance
-	dataManager = DataManager(dataBuffer, processesArgsList)
-	# Events will be used for the dataManager
-	dataManagerEvents = DottedDict(dataManager.getDataManagerEvents())
 	# Create a BoardEventHandler Instance
-	boardEventHandler = BoardEventHandler(board, boardCytonSettings, dataManagerEvents, dataBuffer,writeDataEvent)
+	boardEventHandler = BoardEventHandler(board, boardCytonSettings, newDataAvailable, dataBuffersList, writeDataEvent)
 	# events will be used to control board through any gui
 	boardApiCallEvents = DottedDict(boardEventHandler.getBoardHandlerEvents())
 
 	mode = args.mode[0]
 	if mode == 'pygui':
 
-		# create Process for the dataManager
-		dataManagerProcess = Process(target=dataManager.shareData, args=(shutdownEvent,))
-		processesList.append(dataManagerProcess)
-
 		# create Process for printing Data
-		printData = Process(target=printData,
-		                    args=(board, printDataProcArgs, dataManagerEvents.newDataAvailable, shutdownEvent))
-		# processesList.append(printData)
+		printData = Process(target=printData, name='printData',
+		                    args=(printBuffer, newDataAvailable, shutdownEvent))
+		processesList.append(printData)
 
 		# create Process for the boardEventHandler
-		boardEventHandlerProcess = Process(target=boardEventHandler.start, args=(shutdownEvent,))
+		boardEventHandlerProcess = Process(target=boardEventHandler.start, name='boardEventHandler',
+		                                   args=(shutdownEvent,))
 		processesList.append(boardEventHandlerProcess)
 
 		# create Process for the gui
-		guiProcess = Process(target=startGUI,
-		                     args=(guiProcArgs, dataManagerEvents.newDataAvailable, board, boardApiCallEvents,
+		guiProcess = Process(target=startGUI, name='startGUI',
+		                     args=(guiProcArgs, newDataAvailable, board, boardApiCallEvents,
 		                           boardCytonSettings, shutdownEvent, writeDataEvent))
 		processesList.append(guiProcess)
 
 		# create Process to write data from board to file
-		writeProcess = Process(target=writing, args=(board, writeDataProcArgs, writeDataEvent, shutdownEvent))
+		writeProcess = Process(target=writing, name='writing',
+		                       args=(writingBuffer, windowedDataBuffer, writeDataEvent, shutdownEvent))
 		processesList.append(writeProcess)
+
+		# create Process for the windowing data
+		windowingProcess = Process(target=windowing, name='windowing',
+		                           args=(board, windowingBuffer, windowedDataBuffer, newDataAvailable, shutdownEvent))
+		processesList.append(windowingProcess)
+
 		# start processes in the processList
 		for proc in processesList:
 			proc.start()
