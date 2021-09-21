ed66f9563a5de6badc50760dcde6e37cc2849201
sneakyPent <zaharioudakis@yahoo.com>
2021-09-21 22:06:02 +0300

    changes in training.py - added socketConnection event. When this event set by connectTraining process, only if socket connection established, the startTrainingApp process and the streaming start -when the connection could not be established then wait for 10 sec and then retrying

--- source/training.py
+++ source/training.py
@@ -7,23 +7,30 @@ Consist of 3 method
 used only for the training and in order to run it needs the unity target executable.
 
 """
+import queue
 import socket
 import subprocess
 import sys, os
-from multiprocessing import Process
+import time
+from multiprocessing import Process, Event
 
 sys.path.append('..')
 from utils.constants import Constants as cnst
 
 
-def connectTraining(trainingClassBuffer):
+def connectTraining(trainingClassBuffer, socketConnection):
 	"""
-	Responsible
+	* Responsible
+
 		* to create a socket communication with the target executable.
 		* to receive the training class byte from the open connection.
 		* to put the receiving byte into trainingClassBuffer parameter.
+		* to set the socketConnection event, only if socket connection established. When set,  the :py:meth:`source.training.startTrainingApp` process and the :py:meth:`source.boardEventHandler.BoardEventHandler.startStreaming` can be start.
 
-	:param trainingClassBuffer: {Queue(maxsize=1)} - Buffer used to 'give' the training class to :meth:`source.boardEventHandler.BoardEventHandler.startStreaming`
+	* When the connection could not be established then wait for 10 sec and then retrying.
+
+	:param Queue(maxsize=1) trainingClassBuffer: Buffer used to 'give' the training class to :meth:`source.boardEventHandler.BoardEventHandler.startStreaming`.
+	:param Event socketConnection: Used as flag so the main process :py:meth:`source.training.startTraining` can proceed to start the streaming and the training application.
 
 	"""
 	# create socket
@@ -33,48 +40,66 @@ def connectTraining(trainingClassBuffer):
 
 	# IP and PORT connection
 	port = 8080
-	# s.bind(('139.91.190.32', port)) #local host
-	s.bind(('127.0.0.1', port))  # local host
-	s.listen(30)  # listening for connection for 30 sec?
-	print('socket listensing ... ')
+	while not socketConnection.is_set():
+		try:
+			# s.bind(('139.91.190.32', port)) #local host
+			s.bind(('127.0.0.1', port))  # local host
+			s.listen(30)  # listening for connection for 30 sec?
+			print('socket listensing ... ')
+			socketConnection.set()
+			# try:
+			c, addr = s.accept()  # when port connected
+			print("\ngot connection from ", addr)
 
-	# try:
-	c, addr = s.accept()  # when port connected
-	print("\ngot connection from ", addr)
+			# 1st communication with Quest
+			bytes_received = c.recv(1024)  # received bytes
+			print(bytes_received.decode("utf-8"))
 
-	# 1st communication with Quest
-	bytes_received = c.recv(1024)  # received bytes
-	print(bytes_received.decode("utf-8"))
+			# Send "True" string to start the Quest app
+			nn_output = "True"
+			arr2 = bytes(nn_output, 'utf-8')
+			c.sendall(arr2)  # sending back
 
-	# Send "True" string to start the Quest app
-	nn_output = "True"
-	arr2 = bytes(nn_output, 'utf-8')
-	c.sendall(arr2)  # sending back
-
-	# Quest sends the arrow number
-	bytes_received = c.recv(1).decode("utf-8")  # received bytes
-	print(bytes_received)
-	trainingClassBuffer.put_nowait(bytes_received)
-
-	if bytes_received != "E" and bytes_received != "":
-
-		# q_label.put(int(bytes_received))
-		bytes_received_old = bytes_received
-
-		while True:  # bytes_received != "E": # "E" means end of the action
-
+			# Quest sends the arrow number
 			bytes_received = c.recv(1).decode("utf-8")  # received bytes
+			print(bytes_received)
+			try:
+				trainingClassBuffer.put_nowait(bytes_received)
+			except queue.Full as error:
+				print(error)
+				c.shutdown(socket.SHUT_RDWR)
+				c.close()
+				continue
 
-			if bytes_received == "E" or bytes_received == "":
-				break
-			else:
-				if bytes_received != bytes_received_old:
-					# print(bytes_received)
-					trainingClassBuffer.put_nowait(bytes_received)
-					bytes_received_old = bytes_received
+			if bytes_received != "E" and bytes_received != "":
 
-	c.shutdown(socket.SHUT_RDWR)
-	c.close()
+				# q_label.put(int(bytes_received))
+				bytes_received_old = bytes_received
+
+				while True:  # bytes_received != "E": # "E" means end of the action
+
+					bytes_received = c.recv(1).decode("utf-8")  # received bytes
+
+					if bytes_received == "E" or bytes_received == "":
+						break
+					else:
+						if bytes_received != bytes_received_old:
+							print(bytes_received)
+							try:
+								trainingClassBuffer.put_nowait(bytes_received)
+							except queue.Full:
+								c.shutdown(socket.SHUT_RDWR)
+								c.close()
+								continue
+							bytes_received_old = bytes_received
+
+			c.shutdown(socket.SHUT_RDWR)
+			c.close()
+		except socket.error as error:
+			print(error.__str__() + '. Wait for 10 seconds before trying again')
+			time.sleep(10)
+			pass
+	socketConnection.clear()
 
 
 def startTrainingApp(boardApiCallEvents):
@@ -105,16 +130,27 @@ def startTraining(startTrainingEvent, boardApiCallEvents, _shutdownEvent, traini
 	:param _shutdownEvent:  {Event} - Event used to know when to let every running process terminate
 	:param trainingClassBuffer: {Queue} -  Buffer will be used to 'give' the training class to :meth:`source.boardEventHandler.BoardEventHandler.startStreaming`, via :meth:`source.training.connectTraining`
 	"""
+	socketConnection = Event()
+	socketConnection.clear()
+
+	socketProcess = Process(target=connectTraining, args=(trainingClassBuffer, socketConnection,))
+	applicationProcess = Process(target=startTrainingApp, args=(boardApiCallEvents,))
 	while not _shutdownEvent.is_set():
 		startTrainingEvent.wait(1)
 		if startTrainingEvent.is_set():
-			boardApiCallEvents["startStreaming"].set()
-			socketProcess = Process(target=connectTraining, args=(trainingClassBuffer,))
-			applicationProcess = Process(target=startTrainingApp, args=(boardApiCallEvents,))
+			while not trainingClassBuffer.qsize() == 0:
+				trainingClassBuffer.get_nowait()
 			if not socketProcess.is_alive():
 				socketProcess.start()
-			if not applicationProcess.is_alive():
-				applicationProcess.start()
-			socketProcess.join()
-			applicationProcess.join()
-			startTrainingEvent.clear()
+			socketConnection.wait(1)
+			if socketConnection.is_set():
+				boardApiCallEvents["startStreaming"].set()
+				if not applicationProcess.is_alive():
+					applicationProcess.start()
+				socketProcess.join()
+				applicationProcess.join()
+				startTrainingEvent.clear()
+				socketConnection.clear()
+				# recreating process because eve after Process Termination cannot start a process twice
+				socketProcess = Process(target=connectTraining, args=(trainingClassBuffer, socketConnection,))
+				applicationProcess = Process(target=startTrainingApp, args=(boardApiCallEvents,))
