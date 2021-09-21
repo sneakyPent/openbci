718dcf455422f629c3a08ecb53f674a1b1e1b2f9
sneakyPent <zaharioudakis@yahoo.com>
2021-09-21 19:08:43 +0300

    added documentation in windowing.py

--- source/windowing.py
+++ source/windowing.py
@@ -4,6 +4,19 @@ from utils.coloringPrint import printInfo, printWarning, printError
 
 
 def windowing(board, windowingBuf, windowedData, newDataAvailable, _shutdownEvent):
+	"""
+	* Runs simultaneously with the boardEventHandler process and waits for the newDataAvailable event.
+	* Creates windows according to :py:data:`board` object's windowSize and stepSize.
+	* Puts every created window into the :py:data:`windowedData` buffer.
+	* Every created window is a 3d numpy array as [number of windows][window size][sample size].
+
+	:param board: :py:class:`source.cyton.OpenBCICyton` object created from :py:class:`source.UIManager`.
+	:param Queue windowingBuf: Buffer used for communicating and getting the transmitted data from :py:meth:`source.boardEventHandler.BoardEventHandler.startStreaming`.
+	:param Queue windowedData: Buffer used for communicating and passing the windowed data to :py:meth:`source.writeToFile.writing`.
+	:param Event newDataAvailable: The event the method is waiting for, before proceeding to the next step (windowing).
+	:param Event _shutdownEvent: Used as condition for the method to run.
+	:return: None
+	"""
 	windowCounter = 0
 	currentWindowList = []
 	while not _shutdownEvent.is_set():
@@ -11,7 +24,6 @@ def windowing(board, windowingBuf, windowedData, newDataAvailable, _shutdownEven
 		# the desired package-window size (windowSize*sampleRate) EG: 1*250
 		window = board.getWindow()
 		# the desired step size for each package
-		# FIXME: step is step(sec)*sampling, or step(sec) * windowSize
 		step = board.getWindowStep()
 		if newDataAvailable.is_set():
 			while not windowingBuf.empty():
