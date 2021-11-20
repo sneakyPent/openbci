c3e2e654c19f0cddde1aacc6a4fd188dc416b92f
sneakyPent <zaharioudakis@yahoo.com>
2021-11-20 21:19:00 +0200

    added new filter functions in filters.py - filteringCases: filter function, using different filters based on attributes (filtered: bool, filterType: FilterType, noiseCancellation: bool, order=None) - filterDryElectrodes: filter dry electrodes using filteringCases - filterWetElectrodes: filter wet electrodes using filteringCases

--- utils/filters.py
+++ utils/filters.py
@@ -1,5 +1,7 @@
 from scipy import signal
 import numpy as np
+from brainflow import DataFilter, NoiseTypes, FilterTypes
+from utils.constants import FilterType
 
 
 # Bandpass filter
@@ -24,3 +26,49 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
 	# ************** check the axis!!!
 	y = signal.sosfiltfilt(sos, data, axis=0)
 	return y
+
+
+def filterDryElectrodes(signalData, samplingRate, lowBandBound, highBandBound, order=None):
+	filteredData, _ = filteringCases(signalData, samplingRate, lowBandBound, highBandBound, None, None, filtered=True,
+	                                 filterType=FilterType.butter_bandpass_filter, noiseCancellation=True, order=order)
+	return filteredData
+
+
+def filterWetElectrodes(signalData, samplingRate, lowBandBound, highBandBound, order=None):
+	filteredData, _ = filteringCases(signalData, samplingRate, lowBandBound, highBandBound, None, None, filtered=True,
+	                                 filterType=FilterType.butter_bandpass_filter, noiseCancellation=False, order=order)
+	return filteredData
+
+
+def filteringCases(signalData, samplingRate, lowBandBound, highBandBound, centerFreq, bandwidth,
+                   filtered: bool, filterType: FilterType, noiseCancellation: bool, order=None):
+	data = np.copy(signalData)
+	filterTypeFigureText = ', without filtering'
+	noiseCancellationFigureText = ', without noise cancellation'
+
+	if noiseCancellation:
+		DataFilter.remove_environmental_noise(data, samplingRate, NoiseTypes.FIFTY.value)
+		noiseCancellationFigureText = ', noise Cancellation applied (brainflow remove_environmental_noise)'
+
+	if filtered:
+		if filterType == FilterType.brainflow_bandpass:
+			if order is None:
+				order = 5
+			DataFilter.perform_bandpass(data, samplingRate, centerFreq, bandwidth, order, FilterTypes.BESSEL.value, 1)
+			filterTypeFigureText = ', data filtered with brainflow bandpass centerFreq=' + centerFreq.__str__() + ' bandwidth= ' \
+			                       + bandwidth.__str__() + ' and order=' + order.__str__()
+		elif filterType == FilterType.butter_bandpass_filter:
+			if order is None:
+				order = 10
+			data = butter_bandpass_filter(data, lowBandBound, highBandBound, samplingRate, order=order)
+			filterTypeFigureText = ', data filtered with butter bandpass ' + lowBandBound.__str__() + '-' \
+			                       + highBandBound.__str__() + ' and order=' + order.__str__()
+		elif filterType == FilterType.lowpass_highpass:
+			if order is None:
+				order = 5
+			DataFilter.perform_lowpass(data, samplingRate, highBandBound, order, FilterTypes.BUTTERWORTH.value, 1)
+			DataFilter.perform_highpass(data, samplingRate, lowBandBound, order, FilterTypes.BUTTERWORTH.value, 1)
+			filterTypeFigureText = ', data filtered with highpass ' + lowBandBound.__str__() + ' and lowpass ' \
+			                       + highBandBound.__str__() + ' filters independently and order=' + order.__str__()
+	filterDescription = filterTypeFigureText + noiseCancellationFigureText
+	return data, filterDescription
