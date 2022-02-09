3a8623f52372009360d14ca94bbb2c10175c1990
sneakyPent <zaharioudakis@yahoo.com>
2022-02-09 21:57:41 +0200

    update to allow clasification with non specific number of classifiers

--- classification/classificationOpenBCI.py
+++ classification/classificationOpenBCI.py
@@ -1,3 +1,5 @@
+from distutils import command
+from fileinput import filename
 import os
 from tkinter import filedialog, Tk
 from tkinter.filedialog import asksaveasfilename
@@ -66,138 +68,118 @@ def calculateAccuracy(segment_buffer, chan_ind, fs, frames_ch, lowcut, highcut, 
 # ................................................................
 # ................................................................
 def classify(fileNames, enabledChannels, lowcut, highcut, fs, saveClassifier,subject=None):
-	if len(fileNames) != 4:
-		print('Please choose exactly 4 training files!')
+	electroType = None
+	allWinSignals= None
+	for fileName in fileNames:
+		with h5py.File(fileName, 'r') as fl:
+			# get dataset's windowed data as winSignal
+			winSignal = fl['packages'][:,:,0:9]
+			# get dataset's streaming electrode's settings
+			dtElectroType = fl['StreamSettings'][7,1].decode('UTF-8').split('.')[1]
+			# check whether there is existing electrode type or not. If not init it with the first dataset's settings.
+			# In order to classify, the electrode's type should be the same for each one of the streaming datasets
+			if not electroType:
+				electroType = dtElectroType
+			elif electroType != dtElectroType:
+				printError('Not the same electrodes in every session. No classifier created!')
+				return
+			
+			# add the dataset's windowed signal data into the allWinSignals var
+			if allWinSignals is None:
+				allWinSignals = winSignal
+			else:
+				allWinSignals = np.concatenate((allWinSignals, winSignal))
+	
+	# split the data to train and test
+	X_train, X_test = train_test_split(allWinSignals, test_size=0.35, random_state=42)
+	isExist = os.path.exists(cnst.classifiersDirectory)
+
+	if not isExist:
+		# Create a new directory because it does not exist
+		os.makedirs(cnst.classifiersDirectory)
+	if saveClassifier:
+		classifierFileName = asksaveasfilename(title="Choose a name for the classifier",
+		                                       initialfile=getSessionFilename(classification=True),
+		                                       defaultextension=".sav",
+		                                       filetypes=[('SAV', '.sav')],
+		                                       initialdir=cnst.classifiersDirectory,
+		                                       confirmoverwrite=True)
 	else:
-		file1 = fileNames[0]
-		file2 = fileNames[1]
-		file3 = fileNames[2]
-		file4 = fileNames[3]
-		# open the files
-		with h5py.File(file1, 'r') as f1:
-			with h5py.File(file2, 'r') as f2:
-				with h5py.File(file3, 'r') as f3:
-					with h5py.File(file4, 'r') as f4:
+		classifierFileName = 'rndName'
 
-						dsignal_1 = f1['packages'][:,:,0:9]
-						dsignal_2 = f2['packages'][:,:,0:9]
-						dsignal_3 = f3['packages'][:,:,0:9]
-						dsignal_4 = f4['packages'][:,:,0:9]
-						
-						d1Electrodes = f1['StreamSettings'][7,1].decode('UTF-8').split('.')[1]
-						d2Electrodes = f2['StreamSettings'][7,1].decode('UTF-8').split('.')[1]
-						d3Electrodes = f3['StreamSettings'][7,1].decode('UTF-8').split('.')[1]
-						d4Electrodes = f4['StreamSettings'][7,1].decode('UTF-8').split('.')[1]
-						if d1Electrodes == d2Electrodes == d3Electrodes == d4Electrodes:
-							print (d1Electrodes)
-						else:
-							printError('Not the same electrodes in every session!')
-						
-						# merge the different session data
-						dset = np.concatenate((dsignal_1, dsignal_2, dsignal_3, dsignal_4))
+	if not classifierFileName:
+		return
+	training(X_train, enabledChannels, fs, frames_ch, lowcut, highcut, harmonics_num, _dataInFile,
+	         classifierFileName)
+	acc_LDA, predicted_labels_LDA, ground_truth = calculateAccuracy(X_test, enabledChannels, fs, frames_ch,
+	                                                                lowcut,
+	                                                                highcut, harmonics_num,
+	                                                                _dataInFile, classifierFileName)
 
-						# split the data to train and test
-						X_train, X_test = train_test_split(dset, test_size=0.35, random_state=42)
-						isExist = os.path.exists(cnst.classifiersDirectory)
+	print("")
+	print("************ LDA accuracy is ************** : " + str(acc_LDA))
 
-						if not isExist:
-							# Create a new directory because it does not exist
-							os.makedirs(cnst.classifiersDirectory)
-						if saveClassifier:
-							classifierFileName = asksaveasfilename(title="Choose a name for the classifier",
-							                                       initialfile=getSessionFilename(classification=True),
-							                                       defaultextension=".sav",
-							                                       filetypes=[('SAV', '.sav')],
-							                                       initialdir=cnst.classifiersDirectory,
-							                                       confirmoverwrite=True)
-						else:
-							classifierFileName = 'rndName'
+	
+	# calculate total samples hit and accuracy for each one of the target classes
+	gtArray = np.concatenate( ground_truth, axis=0 ).astype(int)
+	prArray = predicted_labels_LDA.astype(int)
+	commandsResult = targetsAccuracy(gtArray, prArray)
 
-						if not classifierFileName:
-							return
-						training(X_train, enabledChannels, fs, frames_ch, lowcut, highcut, harmonics_num, _dataInFile,
-						         classifierFileName)
-						acc_LDA, predicted_labels_LDA, ground_truth = calculateAccuracy(X_test, enabledChannels, fs, frames_ch,
-						                                                                lowcut,
-						                                                                highcut, harmonics_num,
-						                                                                _dataInFile, classifierFileName)
+	# calculate hits
+	stop = commandsResult['stop']['total']
+	forward = commandsResult['forward']['total']
+	back = commandsResult['back']['total']
+	right = commandsResult['right']['total']
+	left = commandsResult['left']['total']
+	stopHit = commandsResult['stop']['hit']
+	forwardHit = commandsResult['forward']['hit']
+	backHit = commandsResult['back']['hit']
+	rightHit = commandsResult['right']['hit']
+	leftHit = commandsResult['left']['hit']
+	stopAccuracy = commandsResult['stop']['accuracy']
+	forwardAccuracy = commandsResult['forward']['accuracy']
+	backAccuracy = commandsResult['back']['accuracy']
+	rightAccuracy = commandsResult['right']['accuracy']
+	leftAccuracy = commandsResult['left']['accuracy']
+	print("In total: " + str(stop + forward + right + back + left) + " samples")
+	print("Hit Stop: " + str(stopAccuracy) + " , " + str(stopHit) + "/" + str(stop))
+	print("Hit Forward: " + str(forwardAccuracy) + " , " + str(forwardHit) + "/" + str(forward))
+	print("Hit Right: " + str(rightAccuracy) + " , " + str(rightHit) + "/" + str(right))
+	print("Hit Back: " + str(backAccuracy) + " , " + str(backHit) + "/" + str(back))
+	print("Hit Left: " + str(leftAccuracy) + " , " + str(leftHit) + "/" + str(left))
+	print("[Stop, Left, Right, Back, Forward]")
+	print(confusion_matrix(ground_truth, predicted_labels_LDA, labels=[0, 1, 2, 3, 4]))
+	if not saveClassifier:
+		os.remove(classifierFileName)
 
-						print("")
-						print("************ LDA accuracy is ************** : " + str(acc_LDA))
-
-						stop = 0
-						hitStop = 0
-						forward = 0
-						hitForward = 0
-						right = 0
-						hitRight = 0
-						back = 0
-						hitBack = 0
-						left = 0
-						hitLeft = 0
-
-						i = 0
-
-						while i < ground_truth.shape[0]:
-
-							if ground_truth[i] == 0:
-								stop = stop + 1
-								if predicted_labels_LDA[i] == 0:
-									hitStop = hitStop + 1
-								i = i + 1
-
-							elif ground_truth[i] == 4:
-								forward = forward + 1
-								if predicted_labels_LDA[i] == 4:
-									hitForward = hitForward + 1
-								i = i + 1
-
-							elif ground_truth[i] == 2:
-								right = right + 1
-								if predicted_labels_LDA[i] == 2:
-									hitRight = hitRight + 1
-								i = i + 1
-
-							elif ground_truth[i] == 3:
-								back = back + 1
-								if predicted_labels_LDA[i] == 3:
-									hitBack = hitBack + 1
-								i = i + 1
-
-							elif ground_truth[i] == 1:
-								left = left + 1
-								if predicted_labels_LDA[i] == 1:
-									hitLeft = hitLeft + 1
-								i = i + 1
-
-						# calculate hits
-						print("In total: " + str(stop + forward + right + back + left) + " samples")
-						print("Hit Stop: " + str(100 * hitStop / stop) + " , " + str(hitStop) + "/" + str(stop))
-						print("Hit Forward: " + str(100 * hitForward / forward) + " , " + str(hitForward) + "/" + str(
-							forward))
-						print("Hit Right: " + str(100 * hitRight / right) + " , " + str(hitRight) + "/" + str(right))
-						print("Hit Back: " + str(100 * hitBack / back) + " , " + str(hitBack) + "/" + str(back))
-						print("Hit Left: " + str(100 * hitLeft / left) + " , " + str(hitLeft) + "/" + str(left))
-						print("")
-						print("[Stop, Left, Right, Back, Forward]")
-						print(confusion_matrix(ground_truth, predicted_labels_LDA, labels=[0, 1, 2, 3, 4]))
-						if not saveClassifier:
-							os.remove(classifierFileName)
-				
-						classificationResults = {
-							'Subject': subject,
-							'Electrodes': d1Electrodes,
-							'LDA Accuracy': str(acc_LDA),
-							'Total samples': str(stop + forward + right + back + left),
-							'Stop samples': str(hitStop) + "/" + str(stop),
-							'Stop accuracy': str(100 * hitStop / stop),
-							'Forward samples': str(hitForward) + "/" + str(forward),
-							'Forward accuracy': str(100 * hitForward / forward),
-							'Right samples': str(hitRight) + "/" + str(right),
-							'Right accuracy': str(100 * hitRight / right),
-							'Back samples': str(hitBack) + "/" + str(back),
-							'Back accuracy': str(100 * hitBack / back),
-							'Left samples': str(hitLeft) + "/" + str(left),
-							'Left accuracy': str(100 * hitLeft / left)
-						}	
-						return classificationResults
+	classificationResults = {
+		'Subject': subject,
+		'Electrodes': electroType,
+		'channels': [x+1 for x in enabledChannels],
+		'LDA Accuracy': str(acc_LDA),
+		'Total samples': str(stop + forward + right + back + left),
+		'Stop samples': str(stopHit) + " // " + str(stop),
+		'Stop accuracy': str(stopAccuracy),
+		'Forward samples': str(forwardHit) + " // " + str(forward),
+		'Forward accuracy': str(forwardAccuracy),
+		'Right samples': str(rightHit) + " // " + str(right),
+		'Right accuracy': str(rightAccuracy),
+		'Back samples': str(backHit) + " // " + str(back),
+		'Back accuracy': str(backAccuracy),
+		'Left samples': str(leftHit) + " // " + str(left),
+		'Left accuracy': str(leftAccuracy)
+	}	
+	return classificationResults
+		
+		
+def targetsAccuracy(ground_truth, predicted_labels_LDA):
+	commandsResult = {}
+	for cmd, class_ in cnst.targetClassCommands.items():
+		gtIndexes = np.where(ground_truth == class_)[0]
+		predIndexes = np.where(predicted_labels_LDA == class_)[0]
+		commandsResult[cmd] = {
+			'hit': len(np.intersect1d(gtIndexes,predIndexes)),
+			'total': len(gtIndexes),
+			'accuracy': 100 * len(np.intersect1d(gtIndexes,predIndexes)) / len(gtIndexes) 
+		}
+	return commandsResult
